aaefdca901bf2f933408d9d5336e05e8217924e9
godfreyturqz <62551257+godfreyturqz@users.noreply.github.com>
2021-04-12 19:43:21 +0800

    add lazy and suspense function

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
+import { lazy, Suspense } from 'react'
+import 'dotenv/config'
 import './App.css'
 import { BrowserRouter, Switch, Route } from "react-router-dom"
-import Counter from "./Counter"
-import Nav from './Home/Nav'
-import DataFetching from './DataFetching'
-import StateManagement from './StateManagement/StateManagement'
-import Pagination from './Pagination'
-import ReactTable from './ReactTable/ReactTable'
-import Search from './Search'
-import UrlParsing from './UrlParsing'
-import Modal from './Modal'
-import ImageSlider from './ImageSlider'
-import Cloudinary from './Cloudinary'
-import Table from './Table'
-import 'dotenv/config'
+const Counter = lazy(() => import('./Counter'))
+const Nav = lazy(() => import('./Home/Nav'))
+const DataFetching = lazy(() => import('./DataFetching'))
+const StateManagement = lazy(() => import('./StateManagement/StateManagement'))
+const Pagination = lazy(() => import('./Pagination'))
+const ReactTable = lazy(() => import('./ReactTable/ReactTable'))
+const Search = lazy(() => import('./Search')) 
+const UrlParsing = lazy(() => import('./UrlParsing')) 
+const Modal = lazy(() => import('./Modal')) 
+const ImageSlider = lazy(() => import('./ImageSlider')) 
+const Cloudinary = lazy(() => import('./Cloudinary')) 
+const Table = lazy(() => import('./Table')) 
 
+const Loading = () => <div>Loading...</div>
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Nav/>
-        <Switch>
-          <Route path='/counter' component={Counter}/>
-          <Route path='/pagination' component={Pagination}/>
-          <Route path='/datafetching' component={DataFetching}/>
-          <Route path='/reacttable' component={ReactTable}/>
-          <Route path='/search' component={Search}/>
-          <Route path='/statemanagement' component={StateManagement}/>
-          <Route path='/urlparsing/:id' component={UrlParsing}/>
-          <Route path='/modal' component={Modal}/>
-          <Route path='/imageslider' component={ImageSlider}/>
-          <Route path='/cloudinary' component={Cloudinary}/>
-          <Route path='/table' component={Table}/>
-        </Switch>
+        <Suspense  fallback={<Loading/>}>
+          <Nav/>
+          <Switch>
+            <Route path='/counter' component={Counter}/>
+            <Route path='/pagination' component={Pagination}/>
+            <Route path='/datafetching' component={DataFetching}/>
+            <Route path='/reacttable' component={ReactTable}/>
+            <Route path='/search' component={Search}/>
+            <Route path='/statemanagement' component={StateManagement}/>
+            <Route path='/urlparsing/:id' component={UrlParsing}/>
+            <Route path='/modal' component={Modal}/>
+            <Route path='/imageslider' component={ImageSlider}/>
+            <Route path='/cloudinary' component={Cloudinary}/>
+            <Route path='/table' component={Table}/>
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     </div>
   )
